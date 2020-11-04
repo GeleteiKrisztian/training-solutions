5c54d1b7d5b14811dcff26115da19b00c7dc065b
Geletei Krisztian <geleteikrisztian@gmail.com>
2020-11-04 19:56:00 +0100

    1. Modul 4. Feladat 4. Lecke (1-2-3-4)

--- src/main/java/arrayofarrays/ArrayOfArraysMain.java
+++ src/main/java/arrayofarrays/ArrayOfArraysMain.java
@@ -21,9 +21,43 @@ public class ArrayOfArraysMain {
         }
     }
 
+    public int[][] triangularMatrix(int size) {
+        int[][] arrOfArr = new int[size][];
+        for (int i = 0;i < size;i++) {
+            arrOfArr[i] = new int[i+1];
+            for (int j = 0;j < arrOfArr[i].length;j++) {
+                arrOfArr[i][j] = i;
+            }
+        }
+        return arrOfArr;
+    }
+
+    public int[][] getValues() {
+        int[][] arrOfArr = new int[12][];
+        arrOfArr[0] = new int[31];
+        arrOfArr[1] = new int[28];
+        arrOfArr[2] = new int[31];
+        arrOfArr[3] = new int[30];
+        arrOfArr[4] = new int[31];
+        arrOfArr[5] = new int[30];
+        arrOfArr[6] = new int[31];
+        arrOfArr[7] = new int[31];
+        arrOfArr[8] = new int[30];
+        arrOfArr[9] = new int[31];
+        arrOfArr[10] = new int[30];
+        arrOfArr[11] = new int[31];
+        return arrOfArr;
+    }
+
     public static void main(String[] args) {
         ArrayOfArraysMain arrOfArrMain = new ArrayOfArraysMain();
+
         int[][] arrOfArr = arrOfArrMain.multiplicationTable(4);
         arrOfArrMain.printArrayOfArrays(arrOfArr);
+
+        arrOfArr = arrOfArrMain.triangularMatrix(9);
+        arrOfArrMain.printArrayOfArrays(arrOfArr);
+
+        //arrOfArr = arrOfArrMain.getValues();
     }
 }
