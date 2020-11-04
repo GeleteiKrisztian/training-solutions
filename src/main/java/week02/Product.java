b9c35581e18df77cab95ec4e3159017aabbd9421
Geletei Krisztian <geleteikrisztian@gmail.com>
2020-11-04 11:15:52 +0100

    feladat

---
+++ src/main/java/week02/Product.java
@@ -0,0 +1,36 @@
+package week02;
+
+public class Product {
+
+    private String name;
+    private String code;
+
+    public Product(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public boolean areTheyEqual(Product p) {
+        int difference = this.code.length() - p.code.length();
+        boolean lenghtGood = difference >= -1 && difference <= 1;
+        boolean equal = this.name ==p.getName();
+        return equal && lenghtGood;
+    }
+
+    public static void main(String[] args) {
+        Product product1 = new Product("Phone","12345");
+        Product product2 = new Product("Phone","123456");
+        Product product3 = new Product("Monitor","123456789");
+
+        System.out.println(product1.areTheyEqual(product2));
+        System.out.println(product1.areTheyEqual(product3));
+    }
+}
