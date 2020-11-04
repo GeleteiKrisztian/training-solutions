1c2b0b91be6019806a067c8a4febf17155097483
Geletei Krisztian <geleteikrisztian@gmail.com>
2020-11-05 00:38:58 +0100

    1. Modul 4. Feladat 8. Lecke

---
+++ src/main/java/capsules/Capsules.java
@@ -0,0 +1,45 @@
+package capsules;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Capsules {
+
+    private List<String> color = new ArrayList<>();
+
+    public void addLast(String color) {
+        this.color.add(color);
+    }
+
+    public void addFirst(String color) {
+        this.color.add(0,color);
+    }
+
+    public void removeFirst() {
+        color.remove(0);
+    }
+
+    public void removeLast() {
+        color.remove(this.color.size()-1);
+    }
+
+    public List<String> getColors() {
+        return color;
+    }
+
+    public static void main(String[] args) {
+
+        Capsules caps = new Capsules();
+        caps.addLast("Kék");
+        caps.addFirst("Zöld");
+        System.out.println(caps.getColors());
+
+        caps.removeFirst();
+        System.out.println(caps.getColors());
+
+        caps.addLast("Piros");
+        caps.addLast("Fekete");
+        caps.removeLast();
+        System.out.println(caps.getColors());
+    }
+}
