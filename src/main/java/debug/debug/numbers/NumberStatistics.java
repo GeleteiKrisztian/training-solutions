86d20cd6fab432e777b6ef8c8b84d8ec3eb87c42
Geletei Krisztian <geleteikrisztian@gmail.com>
2020-11-05 15:25:00 +0100

    1. Modul 4. Feladat 10. Lecke (2)

--- src/main/java/debug/debug/numbers/NumberStatistics.java
+++ src/main/java/debug/debug/numbers/NumberStatistics.java
@@ -25,7 +25,7 @@ public class NumberStatistics {
     public int minDifferenceBetweenNeighbours() {
         int minDifference = numbers.get(0) - numbers.get(1) >= 0 ? numbers.get(0) - numbers.get(1) : numbers.get(1) - numbers.get(0);
         for(int i = 1; i < numbers.size() - 1; i++) {
-            int actDifference = numbers.get(i) - numbers.get(i + 1);
+            int actDifference = Math.abs(numbers.get(i) - numbers.get(i + 1));
             if(actDifference < minDifference) {
                 minDifference = actDifference;
             }
