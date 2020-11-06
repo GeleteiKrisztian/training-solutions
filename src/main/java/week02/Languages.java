0628615d9e6c747d9d7845cdc9131465a22352b0
Geletei Krisztian <geleteikrisztian@gmail.com>
2020-11-06 10:13:50 +0100

    Feladat

---
+++ src/main/java/week02/Languages.java
@@ -0,0 +1,21 @@
+package week02;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Languages {
+
+    public static void main(String[] args) {
+        List<String> langNames = new ArrayList<>();
+        langNames.add("Java");
+        langNames.add("C++");
+        langNames.add("C#");
+        langNames.add("Python");
+
+        for (String item : langNames) {
+            if (item.length() > 5) {
+                System.out.println(item);
+            }
+        }
+    }
+}
