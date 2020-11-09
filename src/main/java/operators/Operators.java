b596162accc12589b58993256407a94d337c446a
Geletei Krisztian <geleteikrisztian@gmail.com>
2020-11-09 22:40:57 +0100

    1. Modul 6. Fejezet 7. Lecke (1)

---
+++ src/main/java/operators/Operators.java
@@ -0,0 +1,12 @@
+package operators;
+
+public class Operators {
+
+    public boolean isEven(int n) {
+        if (n % 2 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ src/main/java/operators/OperatorsMain.java
@@ -0,0 +1,11 @@
+package operators;
+
+public class OperatorsMain {
+
+    public static void main(String[] args) {
+        Operators operators = new Operators();
+        System.out.println("Páros-e: " + operators.isEven(14));
+        System.out.println("Páros-e: " + operators.isEven(13));
+
+    }
+}
