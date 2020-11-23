f8849480773d8ac81b5c560f41f18c1e6ca16f74
Geletei Krisztian <geleteikrisztian@gmail.com>
2020-11-23 23:56:43 +0100

    1. Modul 9. Fejezet 7. Lecke

---
+++ src/main/java/recursion/FactorialCalculator.java
@@ -0,0 +1,13 @@
+package recursion;
+
+public class FactorialCalculator {
+
+    public long getFactorial(int number) {
+        if (number >= 1) {
+            return number * getFactorial(number - 1);
+        } else {
+            return 1;
+        }
+    }
+
+}
