1532f9f72f3bbf49faa175126318b5e34522c7d5
Geletei Krisztian <geleteikrisztian@gmail.com>
2020-11-25 22:49:49 +0100

    2. Modul 1. Fejezet 4. Lecke (+ bónusz)

---
+++ src/main/java/methodchain/NavigationPoint.java
@@ -0,0 +1,26 @@
+package methodchain;
+
+public class NavigationPoint {
+
+    private int distance;
+    private int azimut;
+
+    public NavigationPoint(int distance, int azimut) {
+        this.distance = distance;
+        this.azimut = azimut;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public int getAzimut() {
+        return azimut;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("distance: %d azimut: %d",distance,azimut);
+    }
+
+}
---
+++ src/main/java/methodchain/Robot.java
@@ -0,0 +1,46 @@
+package methodchain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Robot {
+
+    private List<NavigationPoint> navigationPoints = new ArrayList<>();
+
+    private int distance;
+    private int azimut;
+
+    public List<NavigationPoint> getNavigationList() {
+        return navigationPoints;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public int getAzimut() {
+        return azimut;
+    }
+
+    public Robot go(int meter) {
+        distance += meter;
+        return this;
+    }
+
+    public Robot rotate(int angle) {
+        if (azimut + angle >= 360) {
+            azimut = (azimut + angle) - 360;
+        } else if (azimut + angle <= 0) {
+            azimut = (azimut + angle) + 360;
+        } else {
+            azimut = azimut + angle;
+        }
+        return this;
+    }
+
+    public Robot registerNavigationPoint() {
+        navigationPoints.add(new NavigationPoint(distance,azimut));
+        return this;
+    }
+
+}
