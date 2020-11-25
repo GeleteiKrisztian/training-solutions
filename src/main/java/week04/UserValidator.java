6fed73eb81dfcb75cdb0d90d77e3cc84758551a6
Geletei Krisztian <geleteikrisztian@gmail.com>
2020-11-25 11:19:48 +0100

    03

---
+++ src/main/java/week04/UserValidator.java
@@ -0,0 +1,25 @@
+package week04;
+
+import java.util.List;
+
+public class UserValidator {
+
+    public boolean validate(List<User> users) {
+        for (User item : users) {
+            if (isEmpty(item.getName())) {
+                throw new IllegalArgumentException("Argument null or empty!");
+            }
+            if (item.getAge() < 0 || item.getAge() > 120) {
+                throw new IllegalArgumentException("Invalid argument(s)!");
+            }
+        }
+        return true;
+    }
+
+    private boolean isEmpty(String s) {
+        if (s == null || s.isBlank()) {
+            return true;
+        }
+        return false;
+    }
+}
