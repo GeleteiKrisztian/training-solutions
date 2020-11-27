a0951b96718f47425beed396e50a89bf5315e978
Geletei Krisztian <geleteikrisztian@gmail.com>
2020-11-27 10:57:13 +0100

    05

---
+++ src/main/java/week05/PlayList.java
@@ -0,0 +1,24 @@
+package week05;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayList {
+
+    private List<Song> songs;
+
+    public PlayList(List<Song> songs) {
+        this.songs = new ArrayList<>(songs);
+    }
+
+    public List<Song> findByLengthGreaterThan(int min) {
+        List<Song> resSongs = new ArrayList<>();
+        for (Song item : songs) {
+            if ((double)min > (double)(item.getLengthInSeconds() / 60)) {
+                resSongs.add(item);
+            }
+        }
+        return resSongs;
+    }
+
+}
---
+++ src/main/java/week05/Song.java
@@ -0,0 +1,34 @@
+package week05;
+
+public class Song {
+
+    private String name;
+    private long lengthInSeconds;
+    private String artist;
+
+    public Song(String name, long lengthInSeconds, String artist) {
+        if (lengthInSeconds < 0) {
+            throw new IllegalArgumentException("Length can't be less than 0");
+        }
+        isEmpty(name);
+        isEmpty(artist);
+        this.name = name;
+        this.lengthInSeconds = lengthInSeconds;
+        this.artist = artist;
+    }
+
+    public long getLengthInSeconds() {
+        return lengthInSeconds;
+    }
+
+    private void isEmpty(String s) {
+        if (s == null || s.isBlank()) {
+            throw new IllegalArgumentException("Argument is null or empty!");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s - %s (%d)",artist,name,lengthInSeconds);
+    }
+}
