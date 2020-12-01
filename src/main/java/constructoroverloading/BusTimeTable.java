c0a43f399eb07330990328dfa1f9128202ac4adf
Geletei Krisztian <geleteikrisztian@gmail.com>
2020-12-01 22:50:04 +0100

    2. Modul 2. Fejezet 2. Lecke (3. bónusz)

--- src/main/java/constructoroverloading/BusTimeTable.java
+++ src/main/java/constructoroverloading/BusTimeTable.java
@@ -14,7 +14,9 @@ public class BusTimeTable {
     public BusTimeTable(int firstHour, int lastHour, int everyMinute) {
         List<SimpleTime> times = new ArrayList<>();
         for (int i = firstHour; i <= lastHour; i++) {
-            times.add(new SimpleTime(i,everyMinute));
+            for (int j = everyMinute; j < 60 ;j += everyMinute) {
+                times.add(new SimpleTime(i, everyMinute));
+            }
         }
         startingTimes = times;
     }
@@ -30,6 +32,16 @@ public class BusTimeTable {
             }
         }
         throw new IllegalStateException("No more buses today!");
+    }
+
+    public SimpleTime firstBusStartTime() {
+        SimpleTime simpleTime = null;
+        for (SimpleTime item : startingTimes) {
+            if (simpleTime == null || (item.getHours() * 60 + item.getMinutes()) < (simpleTime.getHours() * 60 + simpleTime.getMinutes())) {
+                simpleTime = item;
+            }
+        }
+        return simpleTime;
     }
 
 }
