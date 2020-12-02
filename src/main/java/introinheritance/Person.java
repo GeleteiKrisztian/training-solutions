99688d72c8ccd8e391d40f6b15c61e93fc28c474
Geletei Krisztian <geleteikrisztian@gmail.com>
2020-12-02 20:08:57 +0100

    2. Modul 3. Fejezet 1. Lecke (1)

---
+++ src/main/java/introinheritance/Person.java
@@ -0,0 +1,25 @@
+package introinheritance;
+
+public class Person {
+
+    private String name;
+    private String address;
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void migrate(String newAddress) {
+        address = newAddress;
+    }
+
+}
