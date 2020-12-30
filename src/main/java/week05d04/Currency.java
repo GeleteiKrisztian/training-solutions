b1fc880792ada8dbb43740fe9958a52d85024548
GeleteiKrisztian <geleteikrisztian@gmail.com>
2020-12-30 21:14:19 +0100

    ...

--- src/main/java/week05d04/Currency.java
+++ src/main/java/week05d04/Currency.java
@@ -1,4 +1,4 @@
-package week05;
+package week05d04;
 
 public enum Currency {
     HUF,USD
--- src/main/java/week05d05/PlayList.java
+++ src/main/java/week05d05/PlayList.java
@@ -1,4 +1,4 @@
-package week05;
+package week05d05;
 
 import java.util.ArrayList;
 import java.util.List;
