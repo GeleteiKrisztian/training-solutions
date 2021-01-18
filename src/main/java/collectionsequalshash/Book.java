f98fb7d7c382bee8668c0dbcc029fba68b5cc648
GeleteiKrisztian <geleteikrisztian@gmail.com>
2021-01-18 14:46:01 +0100

    ...

--- src/main/java/collectionsequalshash/Book.java
+++ src/main/java/collectionsequalshash/Book.java
@@ -38,7 +38,7 @@ public class Book {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Book book = (Book) o;
-        return Objects.equals(regNumber, book.regNumber) && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+        return Objects.equals(title, book.title) && Objects.equals(author, book.author);
     }
 
     @Override
