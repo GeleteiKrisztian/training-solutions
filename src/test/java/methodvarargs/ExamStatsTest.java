fca8b93ecbfcb4958ebda8c545b85061ed119e9a
GeleteiKrisztian <geleteikrisztian@gmail.com>
2021-02-03 21:12:13 +0100

    ...

---
+++ src/test/java/methodvarargs/ExamStatsTest.java
@@ -0,0 +1,12 @@
+package methodvarargs;
+
+import org.junit.jupiter.api.Test;
+
+class ExamStatsTest {
+
+    @Test
+    void nincsTeszt() {
+        System.out.println("Nem volt teszt a githubon! Helyette a megoldás volt a helyén");
+    }
+
+}
