e2805a145e7421f98775643b2074d52045259c61
GeleteiKrisztian <geleteikrisztian@gmail.com>
2021-02-07 18:09:32 +0100

    ...

---
+++ src/main/java/statemachine/TypeWriterState.java
@@ -0,0 +1,20 @@
+package statemachine;
+
+public enum TypeWriterState {
+
+    UPPERCASE {
+        @Override
+        TypeWriterState next() {
+            return TypeWriterState.LOWERCASE;
+        }
+    },
+
+    LOWERCASE {
+        @Override
+        TypeWriterState next() {
+            return TypeWriterState.UPPERCASE;
+        }
+    };
+
+    abstract TypeWriterState next();
+}
