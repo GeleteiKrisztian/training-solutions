dae93464ce525048f6338dedbfd7f1c93d19e737
GeleteiKrisztian <geleteikrisztian@gmail.com>
2021-02-07 18:04:21 +0100

    ...

---
+++ src/main/java/statemachine/TrafficLight.java
@@ -0,0 +1,34 @@
+package statemachine;
+
+public enum TrafficLight {
+
+    RED {
+        @Override
+        TrafficLight next() {
+            return TrafficLight.RED_YELLOW;
+        }
+    },
+
+    RED_YELLOW {
+        @Override
+        TrafficLight next() {
+            return TrafficLight.GREEN;
+        }
+    },
+
+    GREEN {
+        @Override
+        TrafficLight next() {
+            return TrafficLight.YELLOW;
+        }
+    },
+
+    YELLOW {
+        @Override
+        TrafficLight next() {
+            return TrafficLight.RED;
+        }
+    };
+
+    abstract TrafficLight next();
+}
